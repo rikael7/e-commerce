e35d297305eacc91e850276a7caad18017f029f2
rikael7 <rikaelribeiro52@gmail.com>
2026-08-16 00:50:30 -0300

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 
 // =================
 // Import de Middlewares
